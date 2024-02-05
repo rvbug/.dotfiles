9bb8bee63bfca23da7f52ec3195947df92e31fa2
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-02-05 13:43:21 +0530

    Add .wezterm conf

---
+++ .wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = {}
+
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "tokyonight"
+--config.window_background_opacity = 0.9
+return config
