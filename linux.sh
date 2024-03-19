5b1415107bea0aad3dd10d7087d523e99ce5944c
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-03-19 13:11:54 +0530

    yq setup

--- linux.sh
+++ linux.sh
@@ -227,6 +227,9 @@ fi
 
 # install pyyaml 
 $su_user pip3 install pyyaml
+
+# install yq
+echo "installing yq"
 $su_user wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
 if [ $? -eq 0 ]; then
     $su_user chmod +x /usr/local/bin/yq
