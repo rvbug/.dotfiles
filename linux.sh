2becc21bf41ddb0f6f3061d988cb52f3d13ab680
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-03-19 17:20:41 +0530

    code optimization

--- linux.sh
+++ linux.sh
@@ -25,7 +25,7 @@ if [ "$os_name" == "Ubuntu" ]; then
    echo " "
    echo "looks like this is ubuntu"
    echo "####### using apt as your pkg manager"
-   pkg_mgr=apt
+   pkg_mgr=apt-get
    os_name="ubuntu"
 
 elif [ "$os_name" == "Debian GNU/Linux" ]; then
@@ -33,7 +33,7 @@ elif [ "$os_name" == "Debian GNU/Linux" ]; then
    echo "looks like this is debian"
    echo "####### debian uses apt or apt-get pkg manager"
    # echo "at this time, I am unable to get neovim installed on debian"
-   pkg_mgr=apt
+   pkg_mgr=apt-get
    os_name="debian"
 
 elif [ "$os_name" == "Fedora" ]; then
@@ -60,46 +60,6 @@ else
 fi 
 
 
-# if [ -f "/etc/debian_version" ]; then
-#     echo " " 
-#     echo "looks like this is debian"
-#     echo "####### debian uses apt or apt-get pkg manager"
-#     # echo "at this time, I am unable to get neovim installed on debian"
-#     pkg_mgr=apt
-#     os_name="debian"
-# elif [ -f "/etc/fedora-release" ]; then
-#     echo " "
-#     echo "looks like this is fedora"
-#     echo "####### checking for dnf or yum"
-#     dnf > /dev/null 2>&1
-#     if [ $? -eq 0 ]; then
-#       echo " "
-#       echo "dnf is available so using it as your package manager..."
-#       echo " "
-#       pkg_mgr=dnf
-#       os_name="fedora"
-#     else
-#       echo "dnf is unavailable so using yum..."
-#       pkg_mgr=yum
-#       os_name="fedora"
-#     fi
-#
-#  elif [-f "/etc/os-release"]; then
-#      source /etc/os-release
-#      os_name=$NAME
-#      if [ "$os_name" == "Ubuntu" ]; then
-#        echo " "
-#        echo "looks like this is ubuntu"
-#        echo "####### using apt as your pkg manager"
-#      pkg_mgr=apt
-#      os_name="ubuntu"
-#      fi 
-#   else
-#     echo "sorry, I am not sure what this machine is"
-#     echo "exiting the script..."
-#     exit 0
-# fi
-
 echo " "
 echo " "
 echo " "
@@ -108,7 +68,6 @@ echo "####### package manager :  $pkg_mgr"
 echo " "
 echo " "
 echo " "
-
 
 
 # check if you are runing as root
@@ -167,9 +126,8 @@ else
   echo " "
   echo "git is not installed on your machine..."
   echo "installing git..."
-  $pkg_mgr install git -y
+  $su_user $pkg_mgr install git -y
 fi
-
 
 echo " "
 echo "checking if you have wget installed on your machine, if not then install it"
@@ -184,6 +142,20 @@ else
   echo "installing wget..."
   $su_user $pkg_mgr install wget -y
 fi  
+
+echo " "
+echo "checking if you have vim installed on your machine, if not then install it"
+$su_user vim --version > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo " "
+  echo "####### vim is already installed"
+  echo "continuing..."
+else
+  echo " "
+  echo "vim is not installed on your machine..."
+  echo "installing vim..."
+  $su_user $pkg_mgr install vim -y
+fi
 
 if python3 --version > /dev/null; then
   echo "python3 is already installed..."
@@ -201,91 +173,34 @@ if pip --version > /dev/null; then
     echo " "
 else
     echo "pip3 is not installed..."
-    # exit 0
     echo "installing pip3..."
     $su_user $pkg_mgr install python3-pip -y
     echo " "
-    echo "checking if pip3 is installed"
-    pip3 --version > /dev/null
-    echo "pip3 is already installed now ..."
-    echo " "
 fi
-
-# if python3 --version > /dev/null; then
-#   echo "python3 is already installed..."
-#   echo " "
-#   echo "checking for pip3 installation..."
-# elif pip3 --version > /dev/null; then
-#     echo "pip3 is already installed..."
-#     echo " "
-# else
-#     echo "pip3 is not installed..."
-#     # exit 0
-#     echo "installing pip3..."
-#     $su_user $pkg_mgr install python3-pip -y
-# fi
 
 # install pyyaml 
+echo " "
+echo "installing pyyaml using pip3"
 $su_user pip3 install pyyaml
+echo " "
+echo "installing jq using the package manager"
+$su_user $pkg_mgr install jq -y
 
 # install yq
-echo "installing yq"
-$su_user wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-if [ $? -eq 0 ]; then
-    $su_user chmod +x /usr/local/bin/yq
-fi
+# echo "installing yq"
+# $su_user wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+# if [ $? -eq 0 ]; then
+#     $su_user chmod +x /usr/local/bin/yq
+# fi
 
-sudo pip install yq 
-# setting up pkg manager as global variable 
-# pkg_mgr=dnf
+
 
 function install_essentials {
 
-  echo "Package manager is $pkg_mgr"
-  echo "####### checking other essentials..."
-  echo " "
-
-  # echo "####### checking if yq is installed"
-  # if $pkg_mgr list installed yq &>/dev/null; then
-  #   echo " "
-  #   echo "yq already installed..."
-  # else
-  #     echo " "
-  #     echo "##### Installing yq..."
-  #     echo "#######downloaing the package using wget.."
-  #     echo "#######this will take few minutes..." 
-  #     $su_user wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-  #     if [ $? -eq 0 ]; then
-  #       $su_user chmod +x /usr/local/bin/yq
-  #
-  #       echo " " 
-  #       echo "####### checking for the installation..."
-  #       if yq --version &>/dev/null; then
-  #         echo " "
-  #         echo "yq installed successfully..."
-  #       fi
-  #     else
-  #     echo "####### yq installation failed, please manually install this package to proceed with the installation"
-  #     exit 0
-  #     fi
-  # fi
- #
-  # echo " "
-  # read -p "Do you want to update your system?: (y/n) " choice 
-  # case "$choice" in
-  #   y|Y ) echo "updating system..."
-  #         $su_user $pkg_mgr update -y
-  #       ;;
-  #   n|N ) echo "skipping update..."
-  #     ;;
-  #   * ) echo "invalid input, skipping update"
-  #   ;;
-  # esac
-  #  echo " " 
-  #  echo "####### for linux distro using dnf "  
-
-
+   echo "Package manager is $pkg_mgr"
+   echo "####### checking other essentials..."
    echo " "
+
    echo "####### checking if ocaml is already installed..."
 
   # configure_linux
@@ -307,7 +222,6 @@ function install_essentials {
           echo "####### Initialise opam..."
           opam init
       fi
-
   fi
 
 
