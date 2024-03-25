ab5e99e771fcce3a4f9649d6809fca35e95db688
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-03-25 12:46:51 +0530

    project list
    list of projects

---
+++ projects.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+########################################################
+# script      : projects.sh
+# description : select on-going projects  
+# author      : rvbug
+# date        : 15 Aug 2022 : inital commit 
+########################################################
