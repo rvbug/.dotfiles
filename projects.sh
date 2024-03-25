0659714404d84551f75f0492f1bdb8e418aba524
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-03-25 12:53:15 +0530

    project list

--- projects.sh
+++ projects.sh
@@ -6,3 +6,17 @@
 # author      : rvbug
 # date        : 15 Aug 2022 : inital commit 
 ########################################################
+#
+#
+
+root_dir="$HOME/Documents/projects"
+
+
+echo " "
+echo "searching for projects in $root_dir"
+
+
+echo " "
+echo "Select the projects from the list..."
+echo " "
+
