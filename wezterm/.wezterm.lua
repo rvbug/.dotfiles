fa3f1610519dd4a47b937e226a46c8680ac83aca
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-08-06 11:39:59 +0530

    stow update

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = {}
+
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "tokyonight"
+--config.window_background_opacity = 0.9
+config.scrollback_lines = 3500
+config.enable_scroll_bar = true
+
+return config
