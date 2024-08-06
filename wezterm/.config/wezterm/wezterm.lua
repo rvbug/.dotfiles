d2c5bc325f00255611f53ecabbd726e371b47505
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-08-06 17:19:06 +0530

    wezterm update
    updated configuration

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,6 +6,10 @@ local config = wezterm.config_builder()
   color_scheme = "Catppuccin Mocha",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
+  default_cursor_style = "BlinkingBar",
+  -- font = wezterm.font("font-go-mono-nerd-font"),
+  font_size = 12.5,
+
 }
 
 return config
