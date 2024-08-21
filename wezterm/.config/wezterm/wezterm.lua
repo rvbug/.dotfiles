eedf711d48220b06df072485d6e40e9029c25fcf
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-08-21 11:55:49 +0530

    fix wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
  config = {
-  automatic_reload_config = true,
+  -- automatic_reload_config = true,
   color_scheme = "Catppuccin Mocha",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
