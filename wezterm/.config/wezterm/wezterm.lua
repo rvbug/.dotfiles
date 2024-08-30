d9046b9474328daee3f8e1f5befe13586a9799fb
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-08-30 08:10:12 +0530

    minor changes
    changes to bell, window sizes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,12 +3,19 @@ local config = wezterm.config_builder()
 
  config = {
   -- automatic_reload_config = true,
-  color_scheme = "Catppuccin Mocha",
+  -- color_scheme = "Catppuccin Mocha",
+  color_scheme = "Nord (base16)",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   default_cursor_style = "BlinkingBar",
   -- font = wezterm.font("font-go-mono-nerd-font"),
   font_size = 12.5,
+  -- inital size of the window
+  initial_rows = 50,
+  initial_cols = 200,
+
+  -- audio bell 
+  audible_bell = "Disabled",
 
 }
 
