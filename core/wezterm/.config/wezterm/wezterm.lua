4d7915c9464180fb9d17f8a2d3ab66c1ed4a0315
rvbugged <rvbugged@gmail.com>
2025-01-09 08:56:08 +0530

    fix wezterm and tmux

--- core/wezterm/.config/wezterm/wezterm.lua
+++ core/wezterm/.config/wezterm/wezterm.lua
@@ -4,12 +4,19 @@ local config = wezterm.config_builder()
  config = {
   -- automatic_reload_config = true,
   -- color_scheme = "Catppuccin Mocha",
-  color_scheme = "Nord (base16)",
+  -- color_scheme = "Nord (base16)",
+  color_scheme = "Dracula",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   default_cursor_style = "BlinkingBar",
   -- font = wezterm.font("font-go-mono-nerd-font"),
-  font_size = 12.5,
+  -- font = wezterm.font("MesloLGS NF"),
+  font = wezterm.font_with_fallback({
+    "JetBrains Mono",
+    "MesloLGS NF",
+  }),
+  -- font = wezterm.font("JetBrains Mono"),
+  font_size = 14.5,
   -- inital size of the window
   initial_rows = 50,
   initial_cols = 200,
