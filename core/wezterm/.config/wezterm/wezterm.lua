508fc65d4b6c95d5751fa60fa8b8225854ace745
rvbug <rakesh.venkat@gmail.com>
2025-07-16 22:22:24 +0530

    update wezterm

--- core/wezterm/.config/wezterm/wezterm.lua
+++ core/wezterm/.config/wezterm/wezterm.lua
@@ -2,53 +2,30 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
  config = {
-  color_scheme = "Tokyo Night",
+  -- color_scheme = "Kanagawa (Gogh)",
+  color_scheme = 'Kanagawa (Gogh)',
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   default_cursor_style = "BlinkingBar",
-  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font = wezterm.font("Geist Mono"),
   font_size = 14.5,
 
   -- inital size of the window
-  initial_rows = 50,
-  initial_cols = 200,
+  initial_rows = 70,
+  initial_cols = 210,
 
-  -- audio bell 
+  -- audio bell
   audible_bell = "Disabled",
 
-  keys = {
-    {
-      key = 'A',
-      mods = 'CTRL|SHIFT',
-      action = wezterm.action.QuickSelect,
-    },
-  },
+  -- keys = {
+  --   {
+  --     key = 'A',
+  --     mods = 'CTRL|SHIFT',
+  --     action = wezterm.action.QuickSelect,
+  --   },
+  -- },
 
 
 }
 
 return config
-
-
-
-
-  -- automatic_reload_config = true,
-  -- color_scheme = "Catppuccin Mocha",
-  -- color_scheme = "Dracula",
-  -- font = wezterm.font("font-go-mono-nerd-font"),
-
-  -- using aerospace so no need for FullScreen mode
-  -- keys = {
-  --   key = 'n',
-  --   mods = 'SHIFT|CTRL',
-  --   action = wezterm.action.ToggleFullScreen,
-  -- }
-
-
-  -- font = wezterm.font_with_fallback({
-  --   "JetBrains Mono Nerd Font",
-  --   "JetBrains Mono",
-  --   "MesloLGS NF",
-  -- }),
-  -- font = wezterm.font("JetBrains Mono"),
-
